e825ee63e96fb8ef4ff4d0127240639f95a8437e
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-05-18 20:51:54 +0200

    is_prime

--- 01-basic-python/03-loops/02-is-prime/student.py
+++ 01-basic-python/03-loops/02-is-prime/student.py
@@ -1,1 +1,21 @@
 # Write your code here
+# ```javascript
+# function isPrime(n)
+# {
+#     for ( let k = 2; k !== n; ++k )
+#     {
+#         if ( n % k === 0 )
+#         {
+#             return false;
+#         }
+#     }
+
+#     return n > 1;
+# }
+# ```µ
+def is_prime(n):
+    for k in range(2, n):
+        if n % k == 0:
+            return False
+
+    return n > 1

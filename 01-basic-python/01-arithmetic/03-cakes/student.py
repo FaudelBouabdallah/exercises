ce6a5717c1e8d2c1f890647916efba2f887eb2a3
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-05-18 16:26:52 +0200

    cake

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,15 @@
-# Write your code here
+# # Write your code here
+# ```javascript
+# function cakes(eggs, butter, flour)
+# {
+#     // Do NOT rely on floor in your translation
+#     // Use integer division as explained below
+#     const maxByEggs = Math.floor(eggs / 5);
+#     const maxByButter = Math.floor(butter / 250);
+#     const maxByFlour = Math.floor(flour / 5);
+
+#     return Math.min(maxByEggs, maxByButter, maxByFlour);
+# }
+# ```
+def cakes(eggs, butter, flour):
+    return min(eggs//5, butter//250, flour//5)

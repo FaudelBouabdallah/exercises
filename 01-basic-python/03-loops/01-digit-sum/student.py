ce1be88803caf117bdb8aa0549afec5ba1eeb82c
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-05-18 16:52:39 +0200

    digit_sum

--- 01-basic-python/03-loops/01-digit-sum/student.py
+++ 01-basic-python/03-loops/01-digit-sum/student.py
@@ -1,1 +1,23 @@
 # Write your code here
+# ```javascript
+# function digitSum(n)
+# {
+#     let result = 0;
+
+#     while ( n > 0 )
+#     {
+#         result += n % 10;
+#         n = Math.floor(n / 10);
+#     }
+
+#     return result;
+# }
+# ```
+def digit_sum(n):
+    result = 0
+
+    while n > 0:
+        result += n % 10
+        n //= 10
+
+    return result

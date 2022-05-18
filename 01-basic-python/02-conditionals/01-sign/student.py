0f848456143d562c1f0e5707f647bc752d921d3b
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-05-18 16:31:29 +0200

    sign

--- 01-basic-python/02-conditionals/01-sign/student.py
+++ 01-basic-python/02-conditionals/01-sign/student.py
@@ -1,1 +1,25 @@
 # Write your code here
+# ```javascript
+# function sign(n)
+# {
+#     if ( n > 0 )
+#     {
+#         return 1;
+#     }
+#     else if ( n < 0 )
+#     {
+#         return -1;
+#     }
+#     else
+#     {
+#         return 0;
+#     }
+# }
+# ```
+def sign(n):
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    else:
+        return 0

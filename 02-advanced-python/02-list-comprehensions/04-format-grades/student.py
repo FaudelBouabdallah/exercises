e105db26c2b31250abb8ae356931c318632f0ade
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-05-25 13:15:28 +0200

    format_grades

--- 02-advanced-python/02-list-comprehensions/04-format-grades/student.py
+++ 02-advanced-python/02-list-comprehensions/04-format-grades/student.py
@@ -1,1 +1,5 @@
 # Write your code here
+def average(list):
+    return round(sum(list) / len(list))
+def format_grades(grades):
+    return "\n".join(f"{key}: {average(grades)}" for key, grades in grades.items())

6400579f2d55cb14885859c032deec5dfc550984
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-06-11 21:47:14 +0200

    mode

--- 02-advanced-python/02-list-comprehensions/11-mode/student.py
+++ 02-advanced-python/02-list-comprehensions/11-mode/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+def frequencies(ns):
+    result = {}
+    for n in ns:
+        if n not in result:
+            result[n] = 0
+        result[n] += 1
+    return result
+
+def mode(ns):
+    fs = frequencies(ns)
+    return max(fs.items(), key=second)[0]

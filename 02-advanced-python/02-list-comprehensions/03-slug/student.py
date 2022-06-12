8e3d370927b0852df19b39079cbf9776ced90d0d
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-06-12 14:29:36 +0200

    oef

--- 02-advanced-python/02-list-comprehensions/03-slug/student.py
+++ 02-advanced-python/02-list-comprehensions/03-slug/student.py
@@ -1,6 +1,6 @@
 # Write your code here
 def slug(name):
-    parts = name.split()
+    parts = name.split(' ')
     fname = parts[0]
     lname = parts[1:]
-    return "-".join([i.lower() for i in lname + [fname]])
+    return '-'.join(i.lower() for i in lname + [fname])

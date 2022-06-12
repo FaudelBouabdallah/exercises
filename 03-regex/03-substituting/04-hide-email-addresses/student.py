bfb1613b7372f5dcecd4518578b9242b02123ba0
Faudel Bouabdallah <faudel.bouabdallah@student.ucll.be>
2022-06-12 22:07:15 +0200

    oef

--- 03-regex/03-substituting/04-hide-email-addresses/student.py
+++ 03-regex/03-substituting/04-hide-email-addresses/student.py
@@ -4,6 +4,6 @@ import re
 
 def hide_email_addresses(string):
     def replace(match):
-        return '*' * len(match.group(0))
+        return '*' * len(match.group())
         
     return re.sub(r'[a-zA-Z0-9.]+@[a-zA-Z0-9.]+', replace, string)
